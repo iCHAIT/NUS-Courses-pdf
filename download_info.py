bb75776c844defaa7af61530dc2a9d72f1f173b7
iCHAIT <cgupta319@gmail.com>
2017-07-23 01:24:12 +0530

    included more info as available on latest IVLE version

--- download_info.py
+++ download_info.py
@@ -9,14 +9,38 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 cookies = {
     'ASP.NET_SessionId': 'u40bhuancbunufh4lh1i3dwf',
     'UserID': 'e0210471',
-    't': '636362806904372117',
+    't': '636363666858059252',
     'TreeState': '',
-    'ivle12': '43C5F27FBACEA6E42CCB0FC9B03D6C5908DADFA3BFD58FB4BAE021EDE05E005270EAF11251166AF7FCC2DD06367F656093340EBA71325DBD30E524C2A919AC7AEF89DEF6D943387F9B84A19B93D5B5EA5D0C10947B184F989FA192481279F43955DB35F53FEA985474632DD9D0C3099FD60B09A0DFCA7566F8D5190EAF5F41910B281CCCF1F80E253640312510E26024972A849A2BE8108830769F86259EF6CD2D1824DD8AB6EF48B1E2C340C436F60B',
+    'tp': '1758939251',
+    'ivle12': '7DBA69FBBD9B8FAB1F37802EFF78D2F56634B144978F8D9543D6A8C98B31105C3DFF20F36E10B17B0565EBC0CFD196F2CCBA63B1D614C3AECE35FAD7901C84E6EE3B9B626BED0D452621F614CC415BE34437B8C08FBA9A033C29A6899586F7C183486D489ADC7B6CC33172B8449360F30AF6507C50F9F69FA60F346B159C53E3AD7C6B9247C5E3CD48547F13988D2E86164980F217076C31602A066588A4DADF53FC3306A37B2B864EBE045457E1FC7D',
 }
 
 
-# Extract final urls
-with open("final_links.txt", "r") as f:
+### Uncomment the below code to produce html on old IVLE
+# Extract final urls OLD
+# with open("final_links_old.txt", "r") as f:
+#     urls = f.read().splitlines()
+
+
+# for url in urls:
+#     resp = requests.get(url, cookies=cookies)
+#     html = resp.content
+#     soup = BeautifulSoup(html, 'html.parser')
+
+#     # Get the parent level tag and corresponding data - for old version of IVLE
+#     div = soup.find("div", {"id": "ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolder1_UP"})
+
+#     data = str(div)
+
+#     # Dump the data in a master html file
+#     with open("NUS-SOC-Courses-v0.html", "a") as f:
+#         f.write(data)
+
+#     time.sleep(10)
+
+
+# Extract final urls NEW
+with open("final_links_new.txt", "r") as f:
     urls = f.read().splitlines()
 
 
@@ -25,16 +49,69 @@ for url in urls:
     html = resp.content
     soup = BeautifulSoup(html, 'html.parser')
 
-    # Get the parent level tag and corresponding data
-    div = soup.find("div", {"id": "ctl00_ctl00_ContentPlaceHolder1_ContentPlaceHolder1_UP"})
+    # Get the parent level tag and corresponding data - for new version of IVLE
+    div = soup.find("div", {"class": "panel-body"})
+
+
     # Convert to string
     data = str(div)
+    # print(data)
+
+
+    ##### PARSE Lecturer info
+    url_lect = url.replace("default", "list_lecturers")
+
+    resp = requests.get(url_lect, cookies=cookies)
+    html = resp.content
+    soup = BeautifulSoup(html, 'html.parser')
+    div = soup.find("div", {"class": "panel-body"})
+
+    data = data + "<h2>Faculty</h2>" + str(div)
+
+
+
+    ### Todo: Incorporate Reading tabs on IVLE
+
+
+
+    #### Parse Weblinks
+    url_web = url.replace("default", "list_weblinks")
+
+    resp = requests.get(url_web, cookies=cookies)
+    html = resp.content
+    soup = BeautifulSoup(html, 'html.parser')
+    div = soup.find("div", {"class": "well"})
+    table = div.find("table", {"class": "table table-hover table-striped"})
+    rows = table.findAll('tr')
+    tds = [[td.findChildren(text=True) for td in tr.findAll("td")] for tr in rows]
+
+    weblinks = ""
+
+    for lists in tds:
+        if len(lists) != 0:
+            for item in lists:
+                for i in item:
+                    if i != "\n":
+                        weblinks = weblinks + "<br/><br/>" + i.strip()
+
+    data = data + "<h2>Weblinks</h2>" + weblinks
+
+
+
+    #### Parse TimeTable
+    url_time = url.replace("default", "list_timetable")
+
+    resp = requests.get(url_time, cookies=cookies)
+    html = resp.content
+    soup = BeautifulSoup(html, 'html.parser')
+    div = soup.find("div", {"class": "panel-body"})
+
+    data = data + "<h2>TimeTable</h2>" + str(div)
+
+
 
     # Dump the data in a master html file
-    with open("NUS-SOC-Courses.html", "a") as f:
+    with open("NUS-SOC-Courses-v1.html", "a") as f:
         f.write(data)
 
-    time.sleep(10)
-
-    # Finally use pandoc to port html to pdf
-    # Yayy||
+    time.sleep(5)
